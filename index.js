7a8db2d4ed045cf20775c8012747f501f09ada0c
ComputerComa <walkerraydick2@gmail.com>
2024-06-17 18:25:51 -0500

    updates

--- index.js
+++ index.js
@@ -2,22 +2,50 @@ var mode = 0
 var qrs
 var codes
 const QRCode = require('qrcode');
+const fs = require('node:fs')
+const cliProgress = require('cli-progress');
+const progBar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+var codes_len = 0
+var current_code = 0
+const { v4: uuidv4 } = require('uuid'); 
 try {
     qrs = require("./qrs.json")
     codes = qrs.qrs
-    console.log("Qrs.json not found.. Running in interactive mode.")
-    console.log(qrs.qrs)
+    codes_len = codes.length
+    console.log("Qrs.json found.. Running in Automatic mode.")
     mode = 1
 } catch (error) {
     console.log("qrs.json not found.. Running in interactive mode.")
 }
 
-if (mode = 1)
-qrs.qrs.forEach(QRCode => {
-    GenQRCode(QRCode.name,QRCode.content)
-});
+if (mode == 1){
 
-if (mode = 0){
+    const folderName = './OUTPUT'
+
+    try {
+      if (!fs.existsSync(folderName)) {
+        console.log("Creating Output Folder...")
+        fs.mkdirSync(folderName);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    progBar1.start(codes.length,0)
+    codes.forEach(QRCode => {
+        GenQRCode(QRCode.name,QRCode.content)
+        current_code +=1
+        updateProgress(current_code)
+        
+    });
+    progBar1.stop()
+
+
+}
+
+
+
+if (mode == 0){
 
 const prompt = require('prompt-sync')();
 var outfile_name = prompt('File Output Name? (without Extension)')
@@ -27,10 +55,20 @@ GenQRCode(outfile_name,content_out)
 
 
 function GenQRCode(name,content){
-    console.log(`Generating QR Code ${name} with content ${content}`)
+
+    if (name.length == 0 || name == " ") {
+        name = uuidv4()
+    }
+    if (content.length == 0 || content == " "){
+        content = uuidv4()
+    }
     QRCode.toFile(`./OUTPUT/${name}.png`, content, { errorCorrectionLevel: 'H' }, function(err) {
         if (err) throw err;
-        console.log('QR code saved!');
     });
 
 }
+
+function updateProgress(progress){
+progBar1.update(progress)
+}
+
